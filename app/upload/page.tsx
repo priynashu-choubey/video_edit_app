5cdb4076a978582334767b70b59398028b11e8aa
priynashu-choubey <priyanshuchoubey145@gmail.com>
2025-06-20 11:30:23 +0530

    Heder snd Notifications added

---
+++ app/upload/page.tsx
@@ -0,0 +1,4 @@
+"use client";
+
+import VideoUploadForm from "../components/VideoUploadForm";
+export default function VideoUploadPage() {}
--- lib/api-client.ts
+++ lib/api-client.ts
@@ -1,3 +1,5 @@
+import { IVideo } from "@/models/Video";
+export type VideoFormData = Omit<IVideo, "_id">
 type FetchOptions = {
     method?: "GET" | "POST" | "PUT" | "DELETE"
     body?: any
@@ -12,18 +14,28 @@ class ApiClient {
         const { method = "GET", body, headers = {} } = options
 
         const defaultHeaders = {
-            "Content-Type": "application/json"
+            "Content-Type": "application/json",
             ...headers,
 
         }
-        await this.fetch(`/api${endpoint}`, {
+        const response = await fetch(`/api${endpoint}`, {
             method,
             headers: defaultHeaders,
             body: body ? JSON.stringify(body) : undefined
         })
         if (!response.ok) {
-
+            throw new Error(await response.text())
         }
         return response.json()
     }
+    async getVideos() {
+        return this.fetch("/videos")
+    }
+    async createVideo(videoData: VideoFormData) {
+        return this.fetch("/videos", {
+            method: "POST",
+            body: videoData
+        })
+    }
 }
+export const apiClient = new ApiClient();
