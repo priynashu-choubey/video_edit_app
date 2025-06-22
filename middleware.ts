b5233805ce8b4aabd0242f241e01867d17b3e774
priynashu-choubey <priyanshuchoubey145@gmail.com>
2025-06-22 11:03:59 +0530

    Removed Error before deployment

--- middleware.ts
+++ middleware.ts
@@ -1,31 +1,27 @@
-import { tr } from "date-fns/locale";
 import { withAuth } from "next-auth/middleware";
 import { NextResponse } from "next/server";
-import { match } from "node:assert";
-import path from "node:path";
+
 export default withAuth(
-  // Matches the pages config in `[...nextauth]`
   function middleware() {
     return NextResponse.next();
   },
   {
     callbacks: {
       authorized({ req, token }) {
-        // If there is a token, the user is authenticated
         const { pathname } = req.nextUrl;
+
         if (
           pathname.startsWith("/api/auth") ||
           pathname === "/login" ||
           pathname === "/register"
         )
           return true;
+
         if (pathname === "/" || pathname.startsWith("/api/videos")) {
           return true;
         }
-        if (token) {
-          return true; // return !!token; //shortcut *interesting concept
-        }
-        return false;
+
+        return !!token;
       },
     },
   }
